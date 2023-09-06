4db9b06aa04ac2cb3ca5b95e4eb8eec3e34edcca
rodolfo <rodolfojaques13@gmail.com>
2023-09-06 17:59:05 -0300

    feat: Criação pag lista de alarmes

---
+++ src/pages/listaDeAlarmes/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+import { ListaDeAlarmesStl } from "./styles";
+
+function ListaDeAlarmes(){
+    return (
+        <ListaDeAlarmesStl>
+            <Header />
+        </ListaDeAlarmesStl>
+    )
+}
+
+export default ListaDeAlarmes

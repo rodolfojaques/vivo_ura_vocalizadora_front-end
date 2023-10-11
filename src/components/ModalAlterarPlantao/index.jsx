65967bfc32cc66696f5f8915f31b243f675e1df9
joaobuga35 <joaobuga35@gmail.com>
2023-10-11 13:46:15 -0300

    feat: criação do modal alterar plantões mock foi realizado

--- src/components/ModalAlterarPlantao/index.jsx
+++ src/components/ModalAlterarPlantao/index.jsx
@@ -1,6 +1,29 @@
 import { FormModal, ModalContainer, ModalWrapper, TitleModal } from "./styles";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function ModalAlterarPlantao({ setAlterarPlantao }) {
+  const [dateTurns, setDateTurns] = useState([]);
+
+  const handleDateChange = (date) => {
+    const newDateTurn = {
+      date: date,
+      turn: "",
+    };
+    setDateTurns([...dateTurns, newDateTurn]);
+  };
+
+  const handleTurnChange = (event, index) => {
+    const updatedDateTurns = [...dateTurns];
+    updatedDateTurns[index].turn = event.target.value;
+    setDateTurns(updatedDateTurns);
+  };
+
+  const removeDateTurn = (index) => {
+    dateTurns.splice(index, 1);
+    setDateTurns([...dateTurns]);
+  };
   return (
     <ModalWrapper>
       <ModalContainer>
@@ -9,28 +32,44 @@ function ModalAlterarPlantao({ setAlterarPlantao }) {
           <span onClick={() => setAlterarPlantao(false)}>X</span>
         </TitleModal>
         <FormModal>
-          <div>
-            <label htmlFor="data-plantao">Selecione o plantão:</label>
-            <select name="dias-plantao" id="dias-plantao">
-              <option value="">00-00-00</option>
-              <option value="27-02-2023">27-02-2023</option>
-              <option value="27-04-2023">27-04-2023</option>
-              <option value="27-06-2023">27-06-2023</option>
-              <option value="27-08-2023">27-08-2023</option>
-              <option value="27-12-2023">27-12-2023</option>
-            </select>
-          </div>
+          <p>Para alterar o plantão, selecione uma data e um turno.</p>
+          <Calendar onChange={handleDateChange} />
+          <section className="list-datas">
+            {dateTurns.map((dateTurn, index) => (
+              <div className="div-datas" key={index}>
+                <p>
+                  Data: {dateTurn.date.toLocaleDateString()}
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      removeDateTurn(index);
+                    }}
+                    className="btn remover"
+                  >
+                    Remover
+                  </button>
+                </p>
 
-          <div className="div-radio">
-            <label htmlFor="T1">Turno 1</label>
-            <input type="radio" id="T1" name="plantao" value="T1" />
-            <label htmlFor="T2">Turno 2</label>
-            <input type="radio" id="T2" name="plantao" value="T2" />
-            <label htmlFor="T3">Turno 3</label>
-            <input type="radio" id="T3" name="plantao" value="T3" checked />
+                <div className="div-select-turn">
+                  <label>Selecione o turno:</label>
+                  <select
+                    value={dateTurn.turn}
+                    onChange={(event) => handleTurnChange(event, index)}
+                  >
+                    <option value="T1">T1</option>
+                    <option value="T2">T2</option>
+                    <option value="T3">T3</option>
+                  </select>
+                </div>
+              </div>
+            ))}
+          </section>
+          <div className="div-buttons">
+            <button className="btn alterar" type="submit">
+              Alterar
+            </button>
+            <button className="btn cancelar">Cancelar</button>
           </div>
-          <button type="submit">Alterar</button>
-          <button>Cancelar</button>
         </FormModal>
       </ModalContainer>
     </ModalWrapper>
--- src/components/ModalAlterarPlantao/styles.js
+++ src/components/ModalAlterarPlantao/styles.js
@@ -18,7 +18,7 @@ export const ModalWrapper = styled.div`
   align-items: center;
   justify-content: center;
   position: fixed;
-  top: -246px;
+  top: 0;
   z-index: 10;
 `;
 
@@ -31,7 +31,7 @@ export const ModalContainer = styled.div`
   animation: ${slideDown} 1s ease-in-out;
 
   @media (min-width: 768px) {
-    width: 437px;
+    width: 450px;
     height: auto;
   }
 `;
@@ -62,6 +62,10 @@ export const TitleModal = styled.header`
 
 export const FormModal = styled.form`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
   padding: 14px;
   padding-top: 20px;
   height: auto;
@@ -69,39 +73,94 @@ export const FormModal = styled.form`
   border-radius: 5px;
   border: 2px solid var(--gray-0);
 
-  div {
-    display: flex;
-    justify-content: space-between;
-    gap: 8px;
-    align-items: center;
+  p {
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
 
-    label {
-      font-size: 16px;
-      font-weight: 600;
-    }
+  .btn {
+    border-style: none;
+    border-radius: 5px;
 
-    input,
-    select {
-      width: 60%;
-      height: 2rem;
+    width: 6rem;
+    height: 2.5rem;
 
-      padding-left: 0.5rem;
+    font-size: small;
+    font-weight: bold;
 
-      border-style: none;
-      border: solid gray;
-      border-radius: 5px;
+    color: white;
+    cursor: pointer;
+
+    &:active {
+      opacity: 0.5;
     }
   }
 
-  .div-radio{
-      margin-top: 20px;
+  .remover {
+    width: 5rem;
+    height: 24px;
+    background-color: red;
+  }
+
+  .alterar {
+    background-color: purple;
+  }
+
+  .cancelar {
+    background-color: gray;
+  }
+
+  .list-datas {
+    width: 100%;
+    max-height: 250px;
+    overflow-y: auto;
+    margin-top: 20px;
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+
+  .div-datas {
+    margin-top: 12px;
+    margin-bottom: 18px;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+
+    p {
       display: flex;
-      justify-content: flex-start;
+      justify-content: space-between;
       align-items: center;
-
-      input[type="radio"] {
-        width: 20px;
-      }
+      margin-bottom: 16px;
+      font-weight: 600;
     }
+  }
 
+  .div-select-turn {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 15px;
+
+    label {
+      font-size: 18px;
+    }
+  }
+
+  select {
+    width: 50%;
+    height: 2rem;
+    padding-left: 0.5rem;
+    border-style: none;
+    border: solid gray;
+    border-radius: 5px;
+  }
+
+  .div-buttons {
+    margin-top: 12px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 4px;
+  }
 `;

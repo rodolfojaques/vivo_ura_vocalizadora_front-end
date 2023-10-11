e31db7f74c020d291546b1736df873e7efdd351f
joaobuga35 <joaobuga35@gmail.com>
2023-10-11 17:18:58 -0300

    feat: inserindo react toastify nas requisições de login e alterar a senha

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,80 +1,105 @@
-import FormComponent from "../../components/Form"
-import { LoginStl } from "./styles"
+import FormComponent from "../../components/Form";
+import { LoginStl } from "./styles";
 
-import logo from "../../assets/logoProv.png"
-import * as Icon from "react-bootstrap-icons"
+import logo from "../../assets/logoProv.png";
+import * as Icon from "react-bootstrap-icons";
 
-import * as yup from 'yup';
+import * as yup from "yup";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 import axios from "axios";
 import { useContext } from "react";
 import { UserContext } from "../../providers/user";
+import { toast } from "react-toastify";
 
-function Login(){
+function Login() {
+  const history = useHistory();
+  const { baseURL, setUsuario } = useContext(UserContext);
 
-    const history = useHistory()
-    const { baseURL,setUsuario } = useContext(UserContext);
+  const schema = yup.object().shape({
+    RE: yup.string().required("*Campo obrigatório"),
+    password: yup.string().required("*Campo obrigatório"),
+  });
 
-    const schema = yup.object().shape({
-        RE: yup.string().required("*Campo obrigatório"),
-        password: yup.string().required("*Campo obrigatório"),
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const {register, handleSubmit, formState: { errors }} = useForm({
-        resolver: yupResolver(schema)
-    })
+  const formSchema = (data) => {
+    axios
+      .post(`${baseURL}/login`, data)
+      .then((res) => {
+        localStorage.setItem("usuario", JSON.stringify(res.data));
+        setUsuario(res.data);
+        toast.success("Login realizado com sucesso!");
+        history.push("/home");
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.error("RE ou senha incorretos!");
+      });
+  };
 
-    const formSchema = data => {
-
-        axios.post(`${baseURL}/login`,data)
-        .then(res => {
-            localStorage.setItem("usuario",JSON.stringify(res.data))
-            setUsuario(res.data)
-            history.push('/home')
-        })
-        .catch(err => console.error(err)) 
-    }
-
-    return(
-        <LoginStl>
-            <img src={logo} className="logo_login" alt="" />
-            <FormComponent title={"Login"} onSubmit={handleSubmit(formSchema)}>
-                <form action="" className="formClass">
-                    <div className="inputs">
-                        <label htmlFor="" className="labelInput">
-                            <figure>
-                                <Icon.PersonVcardFill size={35}/>
-                            </figure>                            
-                        </label>
-                        <div className="container_err">
-                            <input type="text" className="admin" placeholder="Insira seu usuário..." {...register("RE")}/>
-                            {errors?.RE?.message?
-                            <span className="msg_error">{errors.RE?.message}</span>:""}                            
-                        </div>                        
-                    </div>
-                    <div className="inputs">
-                        <label htmlFor="" className="labelInput">
-                            <figure>
-                                <Icon.UnlockFill size={35} />
-                            </figure>                            
-                        </label>                        
-                        <div className="container_err">
-                            <input type="password" className="password" placeholder="Insira sua senha..." {...register("password")}/>
-                            {errors?.password?.message?
-                            <span className="msg_error">{errors.password?.message}</span>:""}                            
-                        </div>
-                    </div>                    
-                    <input type="submit" className="submit" value="Enter"/>
-                </form>
-            </FormComponent> 
-            <span className="company"><Icon.CCircle size={16}/> Techsolutio 2023</span>           
-        </LoginStl>
-
-    )
+  return (
+    <LoginStl>
+      <img src={logo} className="logo_login" alt="" />
+      <FormComponent title={"Login"} onSubmit={handleSubmit(formSchema)}>
+        <form action="" className="formClass">
+          <div className="inputs">
+            <label htmlFor="" className="labelInput">
+              <figure>
+                <Icon.PersonVcardFill size={35} />
+              </figure>
+            </label>
+            <div className="container_err">
+              <input
+                type="text"
+                className="admin"
+                placeholder="Insira seu usuário..."
+                {...register("RE")}
+              />
+              {errors?.RE?.message ? (
+                <span className="msg_error">{errors.RE?.message}</span>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+          <div className="inputs">
+            <label htmlFor="" className="labelInput">
+              <figure>
+                <Icon.UnlockFill size={35} />
+              </figure>
+            </label>
+            <div className="container_err">
+              <input
+                type="password"
+                className="password"
+                placeholder="Insira sua senha..."
+                {...register("password")}
+              />
+              {errors?.password?.message ? (
+                <span className="msg_error">{errors.password?.message}</span>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+          <input type="submit" className="submit" value="Enter" />
+        </form>
+      </FormComponent>
+      <span className="company">
+        <Icon.CCircle size={16} /> Techsolutio 2023
+      </span>
+    </LoginStl>
+  );
 }
 
-export default Login
+export default Login;

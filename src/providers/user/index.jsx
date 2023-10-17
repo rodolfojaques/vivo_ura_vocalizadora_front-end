eecfb744b700894b0bfbc96c9520d1de939ae80d
rodolfo <rodolfojaques13@gmail.com>
2023-10-17 15:56:58 -0300

    feat: Implementação da logica de criação e deleção de datas de plantões / toast success

--- src/providers/user/index.jsx
+++ src/providers/user/index.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { createContext, useState } from "react";
 
 export const UserContext = createContext();
@@ -6,10 +8,12 @@ function UserProvider({ children }) {
   const baseURL = "http://localhost:3000"
 
   const [ usuario, setUsuario ] = useState(JSON.parse(localStorage.getItem("usuario")))
+  const [ usuarioClicado, setUsuarioClicado ] = useState([])
 
   const [ editSenha, setEditSenha ] = useState(false)
 
   const [alterarPlantao, setAlterarPlantao] = useState(false);
+  const [alterarPlantaoPagUser, setAlterarPlantaoPagUser] = useState(false);
 
   const [idUser, setIdUser] = useState({})
 
@@ -17,11 +21,15 @@ function UserProvider({ children }) {
       <UserContext.Provider value={{
         baseURL,
         usuario,
+        usuarioClicado,
+        setUsuarioClicado,
         setUsuario,
         editSenha, 
         setEditSenha,
         alterarPlantao, 
         setAlterarPlantao,
+        alterarPlantaoPagUser, 
+        setAlterarPlantaoPagUser,
         idUser, 
         setIdUser
       }}>

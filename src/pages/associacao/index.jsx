74b2123296b24746c47580ca22c5317c8b3c9ee0
unknown <joaobuga35@gmail.com>
2023-10-20 14:25:25 -0300

    feat: inserindo container de associações

--- src/pages/associacao/index.jsx
+++ src/pages/associacao/index.jsx
@@ -1,15 +1,18 @@
 import CaminhoComponent from "../../components/Caminho";
+import ContainerGruposComponente from "../../components/ContainerGrupos";
 import Header from "../../components/Header";
 import { AssociacaoStl } from "./styles";
 
-function Associacao(){
-    return(
-        <AssociacaoStl>
-            <Header />
-            <CaminhoComponent path={"Associação"} />
-            Associação
-        </AssociacaoStl>
-    )
+function Associacao() {
+  return (
+    <AssociacaoStl>
+      <Header />
+      <CaminhoComponent path={"Associação"} />
+      <ContainerGruposComponente
+        tipoGrupo={"associacao"}
+      ></ContainerGruposComponente>
+    </AssociacaoStl>
+  );
 }
 
-export default Associacao
+export default Associacao;

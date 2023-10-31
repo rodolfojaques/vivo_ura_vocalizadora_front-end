6f82e4e9b1a6c8e7cb4725cf2ec4f11e2cf1df02
unknown <joaobuga35@gmail.com>
2023-10-31 15:16:34 -0300

    feat: ajuste feito na página de usuários em relação aos campos

--- src/components/TableUsuarios/styles.js
+++ src/components/TableUsuarios/styles.js
@@ -1,91 +1,84 @@
 import styled from "styled-components";
 
 export const TableStl = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding-left: 30px;
+  padding-right: 30px;
 
-    display: flex;
-    flex-direction: column;
-    gap: .5rem;
+  margin-bottom: 2rem;
 
-    margin-bottom: 2rem;
+  .tableAlarmesDiv {
+    overflow-y: hidden;
+  }
 
-    .tableAlarmesDiv{
+  table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    /* width: 100%; */
+    margin: 0px auto;
 
-        overflow-y: hidden;
-
+    tr {
+      text-align: center;
+      display: flex;
+      min-height: 2.4rem;
     }
 
-    table {
-        font-family: Arial, Helvetica, sans-serif;
-        border-collapse: collapse;
-        /* width: 100%; */
-        margin: 0px auto;
-        
-        tr{
-            text-align: center;
-            display: flex;
-            min-height: 2.4rem;
+    tbody {
+      tr {
+        td {
+          white-space: nowrap;
+          min-width: 15rem;
+          overflow: hidden;
+          text-overflow: ellipsis;
+
+          font-size: small;
+
+          span {
+            cursor: pointer;
+          }
         }
-
-        tbody{
-            tr{
-
-                td{
-                    white-space: nowrap;
-                    min-width: 10rem;
-                    overflow: hidden;
-                    text-overflow: ellipsis;
-
-                    font-size: small;
-
-                    span{
-                        cursor: pointer;
-                    }
-                }                
-            }
-        }
-
+      }
     }
-    table td, table th {
-        border: 1px solid #ddd;
-        padding: 8px;
-        min-width: 10rem;
+  }
+  table td,
+  table th {
+    border: 1px solid #ddd;
+    padding: 8px;
+    min-width: 15rem;
+  }
 
-        
+  table tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
 
-    }
+  table tr:hover {
+    background-color: #f0ddee;
+  }
 
-    table tr:nth-child(even){
-        background-color: #f2f2f2;
-    }
+  table th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: purple;
+    color: white;
+  }
 
-    table tr:hover {
-        background-color: #f0ddee;
-    }
+  .btnPaginator {
+    background-color: white;
+    color: purple;
 
-    table th {
-        padding-top: 12px;
-        padding-bottom: 12px;
-        text-align: left;
-        background-color: purple;
-        color: white;
-    }
+    border-style: none;
+    border-radius: 5px;
+    box-shadow: 0 0 1px 0 purple;
 
-    .btnPaginator{
-        background-color: white;
-        color: purple;
+    font-weight: 600;
+    font-size: small;
 
-        border-style: none;
-        border-radius: 5px;
-        box-shadow: 0 0 1px 0 purple;
+    width: 1.3rem;
+    height: 1.3rem;
 
-        font-weight: 600;
-        font-size: small;
-
-        width: 1.3rem;
-        height: 1.3rem;
-
-        cursor: pointer;
-    }
-
-
-`
+    cursor: pointer;
+  }
+`;
